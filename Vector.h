c2f57c653bb2ed371b5fed62e17b1af985054a50
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-05-10 14:39:29 +0900

    関数、構造体を更新

---
+++ Vector.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "Struct.h"
+#include <Novice.h>
+
+//加算
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+
+//減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+
+//スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v);
+
+//内積
+float Dot(const Vector3& v1, const Vector3& v2);
+
+//長さ(ノルム)
+float Length(const Vector3& v);
+
+//正規化
+Vector3 Normalize(const Vector3& v);
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+
+//クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
+
+//正射影ベクトル
+Vector3 Project(const Vector3& v1, const Vector3& v2);
+
+//最近接点
+Vector3 ClosestPoint(const Vector3& point, const Segment& segment);
